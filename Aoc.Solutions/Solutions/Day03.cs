0388c92b81e34bdc98910ac220defc4d91b3716e
Saahil Claypool <saahil@claypools.org>
2020-12-03 18:48:57 -0500

    Finish day03 part 2

--- Aoc.Solutions/Solutions/Day03.cs
+++ Aoc.Solutions/Solutions/Day03.cs
@@ -5,10 +5,14 @@ using Aoc.Runner;
 
 namespace Aoc.Solutions {
     public class Day03 : Day {
-        public int SolveWithSlope(string input, int dx, int dy) {
+        public static int SolveWithSlope(string input, int dx, int dy) {
             var x = 0;
             var hits = input.Split("\n")
-                .Where(line => {
+                .Where((line, idx) => {
+                    if (idx % dy != 0) { // slope > 1 --> only count on dy'th row
+                        return false;
+                    }
+
                     var hit = line[x] == '#';
                     x += dx;
                     x %= line.Length;
@@ -22,7 +26,18 @@ namespace Aoc.Solutions {
             SolveWithSlope(input, 3, 1).ToString();
 
         public override string SolveB(string input) {
-            return "SolveB";
+            List<List<int>> slopes = new() {
+                new() { 1, 1 },
+                new() { 3, 1 },
+                new() { 5, 1 },
+                new() { 7, 1 },
+                new() { 1, 2 }
+            };
+
+            return slopes.Select(slope =>
+                SolveWithSlope(input, slope[0], slope[1])
+            ).Aggregate(1L, (acc, numberHit) => acc * numberHit)
+            .ToString();
         }
         public Day03() {
             Tests = new()
