a2be07cc69d23c7b28132eb89d1d95b32a62adf8
Saahil Claypool <saahil@claypools.org>
2020-12-10 18:18:21 -0500

    Finish part 10B

--- Aoc.Solutions/Solutions/Day10.cs
+++ Aoc.Solutions/Solutions/Day10.cs
@@ -8,15 +8,15 @@ using Extensions;
 
 namespace Aoc.Solutions {
     public class Day10 : Day {
-        static List<BigInteger> Jolts(string input) {
-            var jolts = input.Split("\n").Select(BigInteger.Parse).ToList().Then(l => { l.Sort(); return l; });
+        static List<long> Jolts(string input) {
+            var jolts = input.Split("\n").Select(long.Parse).ToList().Then(l => { l.Sort(); return l; });
             jolts = jolts.Prepend(0).ToList();
             jolts.Add(jolts.Max() + 3);
             return jolts;
         }
         public override string SolveA(string input) {
             var jolts = Jolts(input);
-            var differences = new List<BigInteger>();
+            var differences = new List<long>();
             foreach (var i in Enumerable.Range(0, jolts.Count - 1)) {
                 differences.Add(jolts[i + 1] - jolts[i]);
             }
@@ -29,19 +29,22 @@ namespace Aoc.Solutions {
         }
 
         public override string SolveB(string input) {
-            BigInteger count = 1;
             var jolts = Jolts(input);
             jolts.Reverse();
-            var waysToReach = new List<BigInteger>();
+            var subtreeCounts = new Dictionary<long, long>();
             foreach (var jolt in jolts) {
+                var possibleNext = jolts.Where(j => j > jolt && j <= jolt + 3);
+                subtreeCounts[jolt] = possibleNext.Select(n => subtreeCounts[n]).Sum();
+                if (subtreeCounts[jolt] == 0) subtreeCounts[jolt] = 1;
             }
-            return count.ToString();
+            return subtreeCounts[0].ToString();
         }
         public Day10() {
             Tests = new()
             {
                 new("1", "long", "220", input => GetInput(input).Then(SolveA)),
-                new("b", "long", "19208", input => GetInput(input).Then(SolveB))
+                new("b", "long", "19208", input => GetInput(input).Then(SolveB)),
+                new("bshort", "short", "8", input => GetInput(input).Then(SolveB))
             };
 
         }
