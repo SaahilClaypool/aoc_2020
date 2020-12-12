ce56e0ef18249b1bfaa8a3b0ca1e1ac719764f2c
Saahil Claypool <saahil@claypools.org>
2020-12-12 15:29:07 -0500

    Part B

--- Aoc.FsSolutions/Solutions/Day12.fs
+++ Aoc.FsSolutions/Solutions/Day12.fs
@@ -5,14 +5,14 @@ open System.Collections.Generic
 open System.Collections
 
 
-type Command = { Command: char; Val: int }
-type Loc = { Row: int; Col: int }
+type Command = { Command: char; Val: int64 }
+type Loc = { Row: int64; Col: int64 }
 
 type Ship = { Direction: char; Location: Loc }
 
 let ParseInput (input: string) =
     let parseLine =
-        fun (x: string) -> { Command = x.[0]; Val = int x.[1..] }
+        fun (x: string) -> { Command = x.[0]; Val = int64 x.[1..] }
 
     input.Split("\n") |> Seq.map parseLine
 
@@ -42,19 +42,20 @@ let stepDirection ship dir value =
     | _ -> ship
 
 let turn ship degrees =
-    let currentDegrees = 
+    let currentDegrees =
         match ship.Direction with
-        | 'E' -> 0
-        | 'N' -> 90
-        | 'W' -> 180
-        | 'S' -> 270
-        | _ -> 0
-    let normalized = (degrees + currentDegrees + 360) % 360
+        | 'E' -> 0L
+        | 'N' -> 90L
+        | 'W' -> 180L
+        | 'S' -> 270L
+        | _ -> 0L
+
+    let normalized = (degrees + currentDegrees + 360L) % 360L
     match normalized with
-    | 0 -> { ship with Direction = 'E' }
-    | 90 -> { ship with Direction = 'N' }
-    | 180 -> { ship with Direction = 'W' }
-    | 270 -> { ship with Direction = 'S' }
+    | 0L -> { ship with Direction = 'E' }
+    | 90L -> { ship with Direction = 'N' }
+    | 180L -> { ship with Direction = 'W' }
+    | 270L -> { ship with Direction = 'S' }
     | _ -> ship
 
 
@@ -75,8 +76,73 @@ let Simulate input =
     |> Seq.fold
         step
            { Direction = 'E'
-             Location = { Row = 0; Col = 0 } }
+             Location = { Row = 0L; Col = 0L } }
 
+
+// ----- PART B
+
+type State = { Waypoint: Loc; Ship: Ship }
+
+let move location direction value =
+    match direction with
+    | 'E' ->
+        { location with
+              Col = location.Col + value }
+    | 'N' ->
+        { location with
+              Row = location.Row + value }
+    | 'W' ->
+        { location with
+              Col = location.Col - value }
+    | 'S' ->
+        { location with
+              Row = location.Row - value }
+    | _ -> location
+
+let rotate state degrees =
+    let newRotation = (degrees + 360L) % 360L
+    match newRotation with
+    | 90L ->
+        { Row = state.Waypoint.Col
+          Col = -state.Waypoint.Row }
+    | 180L ->
+        { Row = -state.Waypoint.Row
+          Col = -state.Waypoint.Col }
+    | 270L ->
+        { Row = -state.Waypoint.Col
+          Col = state.Waypoint.Row }
+    | _ -> state.Waypoint
+
+
+let moveToWaypoint waypoint shiploc value =
+    let newWaypoint =
+        { Row = waypoint.Row * value
+          Col = waypoint.Col * value }
+
+    { Row = shiploc.Row + newWaypoint.Row
+      Col = shiploc.Col + newWaypoint.Col }
+
+
+let stepB state (command: Command) =
+    match command.Command with
+    | 'N'
+    | 'S'
+    | 'E'
+    | 'W' ->
+        { state with
+              Waypoint = move state.Waypoint command.Command command.Val }
+    | 'F' ->
+        { state with
+              Ship =
+                  { state.Ship with
+                        Location = moveToWaypoint state.Waypoint state.Ship.Location command.Val } }
+    | 'R' ->
+        { state with
+              Waypoint = rotate state -command.Val }
+    | 'L' ->
+        { state with
+              Waypoint = rotate state command.Val }
+    | _ -> state
 
 
 type Day12() =
@@ -84,10 +150,24 @@ type Day12() =
 
     override this.SolveA(input) =
         let ship = Simulate input
-        string (abs(ship.Location.Row) + abs(ship.Location.Col))
+        string (abs (ship.Location.Row) + abs (ship.Location.Col))
 
-    override this.SolveB(input) = "B"
+    override this.SolveB(input) =
+        let state =
+            ParseInput input
+            |> Seq.fold
+                stepB
+                   { Waypoint = { Col = 10L; Row = 1L }
+                     Ship =
+                         { Direction = 'E'
+                           Location = { Row = 0L; Col = 0L } } }
+
+        string
+            (abs (state.Ship.Location.Row)
+             + abs (state.Ship.Location.Col))
+
+
 
     override this.Tests =
-        [ Test("fsharp test", "123", "123", (fun input -> "123")) ]
+        [ Test("sample 12", "sample", "286", (fun input -> (this.GetInput >> this.SolveB) input)) ]
         |> List<Test>
