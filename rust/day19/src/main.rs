91003f63b34754c0040753db6fbffe61e5c496a4
Saahil Claypool <saahil@claypools.org>
2020-12-21 11:52:55 -0500

    Finish day 19 part B

--- rust/day19/src/main.rs
+++ rust/day19/src/main.rs
@@ -2,18 +2,17 @@ use std::{collections::HashMap, fs};
 
 fn main() {
     println!("Hello, world!");
+    part_a();
+    part_b();
 
-    let input = "./small_input.txt";
-    let input = "./sample.txt";
+}
+
+fn part_a() {
     let input = "./input.txt";
     let contents = fs::read_to_string(input).expect("Something went wrong reading the file");
 
     let (rules, lines) = parse(&contents);
 
-    println!("{}", contents);
-    println!("{:?}", rules);
-
-    println!("matching..");
     let matching_lines: Vec<&str> = lines
         .into_iter()
         .filter(|line| {
@@ -29,6 +28,31 @@ fn main() {
     // dbg!(visited);
     println!("number of matches: {}", matching_lines.len());
 }
+
+fn part_b() {
+    let input = "./input.txt";
+    let contents = fs::read_to_string(input).expect("Something went wrong reading the file");
+
+    let (mut rules, lines) = parse(&contents);
+    rules.insert(8, Rule::Reference(vec![vec![42], vec![42, 8]]));
+    rules.insert(11, Rule::Reference(vec![vec![42, 31], vec![42, 11, 31]]));
+
+    let matching_lines: Vec<&str> = lines
+        .into_iter()
+        .filter(|line| {
+            let mut visited = &mut HashMap::new();
+            let m: Vec<usize> = matches(0, line, 0, &rules, &mut visited)
+                .into_iter()
+                .filter(|m| *m == line.len())
+                .collect();
+            m.len() > 0
+        })
+        .collect();
+
+    // dbg!(visited);
+    println!("number of matches: {}", matching_lines.len());
+}
+
 
 #[derive(Debug)]
 enum Rule {
@@ -51,18 +75,15 @@ fn matches<'a>(
 
 
     if input_idx >= input.len() {
-        // println!("sc: Out of bounds");
         return vec![];
     }
 
     let state = state_string(input, input_idx, ridx);
     match visited.get(state.as_str()) {
         Some(previous_computation) => {
-            // println!("sc: previous state bubbling up {} {:?}", state.as_str(), &previous_computation);
             return previous_computation.to_vec()
         },
         None => {
-            // println!("sc: temp insert into visited for rule {}", ridx);
             visited.insert(state.clone(), vec![]);
         }
     }
@@ -71,7 +92,6 @@ fn matches<'a>(
     let rule = rules.get(&ridx).unwrap();
     let possible_indexes = match rule {
         Rule::Rule(letter) => {
-            // println!("sc: Leaf => {} @ {}", letter, input_idx);
             if *letter == input.chars().nth(input_idx).expect("out of bounds") {
                 vec![input_idx + 1]
             } else {
@@ -81,13 +101,11 @@ fn matches<'a>(
         Rule::Reference(referenced_rules) => referenced_rules
             .iter()
             .flat_map(|rule_list| {
-                // println!("sc: Arm {:?}", rule_list);
                 match_list(rule_list, input, input_idx, rules, visited)
             })
             .collect(),
     };
 
-    // println!("sc: inserting {:?} into visited for rule {}", possible_indexes.clone(), ridx);
     visited.insert(state, possible_indexes.clone());
     possible_indexes
 }
